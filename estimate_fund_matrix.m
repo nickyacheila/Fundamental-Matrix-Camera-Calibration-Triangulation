7dde69524589ec2c5d0b1c5bf9f45c26625edcca
nik <nacheila@yahoo.com>
2019-06-03 20:12:52 +0200

    fixed bug

--- estimate_fund_matrix.m
+++ estimate_fund_matrix.m
@@ -1,7 +1,6 @@
 function [F_matrix] = estimate_fund_matrix(matches)
 
 % create matrix A
-
 A=ones(size(matches,1),9);
 A(:,1)=matches(:,1).*matches(:,3);
 A(:,2)=matches(:,1).*matches(:,4);
@@ -11,20 +10,21 @@ A(:,5)=matches(:,2).*matches(:,4);
 A(:,6)=matches(:,2);
 A(:,7)=matches(:,3);
 A(:,8)=matches(:,4);
+
+%first SVD
 [U,S,V]=svd(A);
+%find min singular value
 [M,I]=min(diag(S));
-Vt=V';
-f=Vt(:,I);
+f=V(:,I);
 
 F_matrix1=reshape(f,3,3);
 
 [U1,S1,V1]=svd(F_matrix1);
-
 [M1,I1]=min(diag(S1));
 
 S1(I1,I1)=0;
 
-F_matrix=U1*S1*V1;
+F_matrix=U1*S1*V1';
 f1=reshape(F_matrix,9,1);
 Z=A*f1;
 disp(Z);
