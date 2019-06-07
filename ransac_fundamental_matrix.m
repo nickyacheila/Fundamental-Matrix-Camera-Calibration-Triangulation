ac0f55b7c0c098c8eab54dc8602547b66b3c1545
Giorgos Patsiaouras <giorgospatsiaouras@gmail.com>
2019-06-07 14:05:57 +0200

    refactored a bit

--- ransac_fundamental_matrix.m
+++ ransac_fundamental_matrix.m
@@ -1,4 +1,4 @@
-function [F_matrix, matched_points_a, matched_points_b] = ransac_fundamental_matrix(matches1, matches2)
+function [best_fund_matrix, matched_points_a, matched_points_b] = ransac_fundamental_matrix(matches1, matches2)
 s=8;
 d_threshold=1;
 T= size(matches1,1)*70/100;
@@ -6,26 +6,39 @@ max_inliers=0;
 best_fund_matrix=zeros(3,3);
 
 for i =1:1000
+    % Initiate distances to -1
     distances=-ones(size(matches1,1),1);
-     s_ind=randperm(size(matches1,1),s);
-     fund_matrix=estimate_fund_matrix([matches1(s_ind,:) matches2(s_ind,:)]);
-     
-     for j=1:size(matches1,1)
-        distances(j)=abs([matches1(j,:) 1]*fund_matrix*[matches2(j,:) 1]');
-     end
-     inliers_ind=distances(distances<d_threshold);
-     
-     inliers_size=size(inliers,1);
-     
-     if inliers_size>T
-         if inliers_size>max_inliers
-             max_inliers=inliers_size;
-             best_fund_matrix=fund_matrix;
-         end
-     end
+    
+    % Get the indices of the sample points
+    s_indices=randperm(size(matches1,1),s);
+    
+    % Calculate the fundamental matrix from the sampled points
+    fund_matrix=estimate_fund_matrix([matches1(s_indices,:) matches2(s_indices,:)]);
+    
+    % Iterate over matching points
+    for j=1:size(matches1,1)
+        % Calculate distance of point from model
+        distance = abs([matches1(j,:) 1]*fund_matrix*[matches2(j,:) 1]');
+        
+        % Update distance array only if the threshold is met
+        if distance < d_threshold
+            distances(j)=distance;
+        end
+    end
+    
+    % get number of inliers for this model
+    inliers_size=nnz(distances>-1);
+    
+    % if the inliers are more than threshold and more than the max_inliers
+    % update the max values of fundamental matrix and inliers found
+    if inliers_size>T && inliers_size>max_inliers
+        max_inliers=inliers_size;
+        best_fund_matrix=fund_matrix;
+    end
 
 end
-%fit
+
+% What is returned values matched_points_a and matched_points_b
 
 end
 
